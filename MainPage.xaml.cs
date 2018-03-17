513b0c670dd7d1d05bbb45fb38d7e52e7e990928
pawe9N <zerratul@wp.pl>
2018-03-17 22:18:07 +0100

    Added first translation method

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +28,33 @@ namespace Vocabulary_Translator_App
         public MainPage()
         {
             this.InitializeComponent();
+        }
+        public async Task<string> TranslateText(string input, string languagePair)
+        {
+            WebRequest request = WebRequest.Create(String.Format("http://www.google.com/translate_t?hl=en&ie=UTF8&text={0}&langpair={1}", input, languagePair));
+            WebResponse response = await request.GetResponseAsync(); ;
+            Stream data = response.GetResponseStream();
+            string html = String.Empty;
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            using (StreamReader stream = new StreamReader(data, Encoding.GetEncoding(28592)))
+            {
+                html = stream.ReadToEnd();
+            }
+            html = html.Substring(html.IndexOf("<span title=\"") + "<span title=\"".Length);
+            html = html.Substring(html.IndexOf(">") + 1);
+            html = html.Substring(0, html.IndexOf("</span>"));
+
+            return html;
+
+        }
+
+        private void Translate_Click(object sender, RoutedEventArgs e)
+        {
+            string toTranslateText = ToTranslateText.Text;
+
+            var translatedText = Task.Run(() => TranslateText(toTranslateText, "en|pl")).Result;
+
+            TranslatedText.Text = translatedText;
         }
     }
 }
