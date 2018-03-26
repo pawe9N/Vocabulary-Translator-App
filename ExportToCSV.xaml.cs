61ae6175d9383a6fb3bfcb19dea062d6dff92857
pawe9N <zerratul@wp.pl>
2018-03-27 01:45:40 +0200

    Added export to csv

--- ExportToCSV.xaml.cs
+++ ExportToCSV.xaml.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Text;
+using Vocabulary_Translator_App.Class;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +17,100 @@ namespace Vocabulary_Translator_App
     /// </summary>
     public sealed partial class ExportToCSV : Page
     {
+        //folder where to export csv
+        StorageFolder folder;
         public ExportToCSV()
         {
             this.InitializeComponent();
+            this.SettingDictionaries();
+        }
+
+        //show folderpicker dialog
+        private async void ShowFolderPickerDialog(object sender, RoutedEventArgs e)
+        {
+            FolderPicker fp = new FolderPicker()
+            {
+                SuggestedStartLocation = PickerLocationId.Desktop,     
+            };
+
+            fp.FileTypeFilter.Add("*");
+
+            folder = await fp.PickSingleFolderAsync();
+            if(folder != null)
+                ChoosenPathText.Text = folder.Path;
+
+            EnableExport(ChoosenDictionary.Text, ChoosenPathText.Text);
+        }
+
+        //enable button to make export to csv
+        private void EnableExport(string directory, string path)
+        {
+            if(directory != "You choose ... dictionary" && path != "You choose ... path")
+            {
+                ExportButton.IsEnabled = true;
+            }
+        }
+
+        //setting dictionaries flyout
+        private void SettingDictionaries()
+        {
+            DatabaseConnection conDB = new DatabaseConnection();
+            List<string> dictionaries = conDB.GettingTables();
+
+            foreach (string dictionary in dictionaries)
+            {
+                CreatingMenuFlyoutItem(DictionariesMenu, StringOperation.TableToLanguages(dictionary));
+            }
+        }
+
+        //creating single menu item
+        private void CreatingMenuFlyoutItem(MenuFlyout DictionariesMenu, string text)
+        {
+            MenuFlyoutItem item = new MenuFlyoutItem()
+            {
+                Text = text
+            };
+            item.Click += ChoosingDictionary;
+            DictionariesMenu.Items.Add(item);
+        }
+
+        //choosing dictionary
+        private void ChoosingDictionary(object sender, RoutedEventArgs e)
+        {
+            MenuFlyoutItem item = (sender as MenuFlyoutItem);
+            ChoosenDictionary.Text = item.Text;
+
+            EnableExport(ChoosenDictionary.Text, ChoosenPathText.Text);
+        }
+
+        //making export from database to csv file
+        private async void ExportToCSVFile(object sender, RoutedEventArgs e)
+        {
+            string dictionary = StringOperation.LanguagesToTable(ChoosenDictionary.Text);
+            string path = Path.Combine(ChoosenPathText.Text, dictionary+".csv");
+            
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Word, Translation");
+
+            DatabaseConnection dbCon = new DatabaseConnection();
+            List<string> words = dbCon.GettingRows(dictionary);
+
+            foreach(string word in words)
+            {
+                sb.AppendLine(word.Replace(" ➤", ","));
+            }
+
+            try
+            {
+                await folder.GetFileAsync(dictionary + ".csv");
+            }
+            catch
+            {
+                await folder.CreateFileAsync(dictionary + ".csv");
+            }
+
+            StorageFile csvFile = await folder.GetFileAsync(dictionary + ".csv");
+            await FileIO.WriteTextAsync(csvFile,  sb.ToString(), Windows.Storage.Streams.UnicodeEncoding.Utf8);
         }
     }
 }
